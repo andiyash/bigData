a94a0d7f64fc109256382780d212c4842a6b4012
Andrey Yashakin <yashakin2073@gmail.com>
2023-12-24 17:02:36 +0400

    Сделал третью лабораторную

---
+++ Lab3/LongRidesExercise.java
@@ -0,0 +1,168 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.training.exercises.longrides;
+
+import org.apache.flink.annotation.VisibleForTesting;
+import org.apache.flink.api.common.JobExecutionResult;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.state.ValueState;
+import org.apache.flink.api.common.state.ValueStateDescriptor;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
+import org.apache.flink.streaming.api.functions.sink.PrintSinkFunction;
+import org.apache.flink.streaming.api.functions.sink.SinkFunction;
+import org.apache.flink.streaming.api.functions.source.SourceFunction;
+import org.apache.flink.training.exercises.common.datatypes.TaxiRide;
+import org.apache.flink.training.exercises.common.sources.TaxiRideGenerator;
+import org.apache.flink.util.Collector;
+
+import java.time.Duration;
+
+/**
+ * The "Long Ride Alerts" exercise.
+ *
+ * <p>The goal for this exercise is to emit the rideIds for taxi rides with a duration of more than
+ * two hours. You should assume that TaxiRide events can be lost, but there are no duplicates.
+ *
+ * <p>You should eventually clear any state you create.
+ */
+public class LongRidesExercise {
+    private final SourceFunction<TaxiRide> source;
+    private final SinkFunction<Long> sink;
+
+    /** Creates a job using the source and sink provided. */
+    public LongRidesExercise(SourceFunction<TaxiRide> source, SinkFunction<Long> sink) {
+        this.source = source;
+        this.sink = sink;
+    }
+
+    /**
+     * Creates and executes the long rides pipeline.
+     *
+     * @return {JobExecutionResult}
+     * @throws Exception which occurs during job execution.
+     */
+    public JobExecutionResult execute() throws Exception {
+
+        // set up streaming execution environment
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        // start the data generator
+        DataStream<TaxiRide> rides = env.addSource(source);
+
+        // the WatermarkStrategy specifies how to extract timestamps and generate watermarks
+        WatermarkStrategy<TaxiRide> watermarkStrategy =
+                WatermarkStrategy.<TaxiRide>forBoundedOutOfOrderness(Duration.ofSeconds(60))
+                        .withTimestampAssigner(
+                                (ride, streamRecordTimestamp) -> ride.getEventTimeMillis());
+
+        // create the pipeline
+        rides.assignTimestampsAndWatermarks(watermarkStrategy)
+                .keyBy(ride -> ride.rideId)
+                .process(new AlertFunction())
+                .addSink(sink);
+
+        // execute the pipeline and return the result
+        return env.execute("Long Taxi Rides");
+    }
+
+    /**
+     * Main method.
+     *
+     * @throws Exception which occurs during job execution.
+     */
+    public static void main(String[] args) throws Exception {
+        LongRidesExercise job =
+                new LongRidesExercise(new TaxiRideGenerator(), new PrintSinkFunction<>());
+
+        job.execute();
+    }
+
+    @VisibleForTesting
+    public static class AlertFunction extends KeyedProcessFunction<Long, TaxiRide, Long> {
+
+        private transient ValueState<TaxiRide> rideState;
+
+        @Override
+        public void open(Configuration config) {
+            // Инициализация состояния ValueState в методе open
+            ValueStateDescriptor<TaxiRide> rideStateDescriptor =
+                    new ValueStateDescriptor<>("событие поездки", TaxiRide.class);
+            rideState = getRuntimeContext().getState(rideStateDescriptor);
+        }
+
+        @Override
+        public void processElement(TaxiRide ride, Context context, Collector<Long> out) throws Exception {
+
+            TaxiRide firstRideEvent = rideState.value();
+
+            if (firstRideEvent == null) {
+                // Запоминаем первое событие поездки
+                rideState.update(ride);
+
+                if (ride.isStart) {
+                    // Устанавливаем таймер для проверки слишком долгих поездок
+                    context.timerService().registerEventTimeTimer(getTimerTime(ride));
+                }
+            } else {
+                if (ride.isStart) {
+                    // Проверяем, слишком ли долгая поездка
+                    if (rideTooLong(ride, firstRideEvent)) {
+                        out.collect(ride.rideId);
+                    }
+                } else {
+                    // Удаляем таймер для первой поездки, так как встречено событие END
+                    context.timerService().deleteEventTimeTimer(getTimerTime(firstRideEvent));
+
+                    // Проверяем, не стала ли поездка слишком долгой
+                    if (rideTooLong(firstRideEvent, ride)) {
+                        out.collect(ride.rideId);
+                    }
+                }
+
+                // Очищаем состояние, так как оба события были учтены
+                rideState.clear();
+            }
+        }
+
+        @Override
+        public void onTimer(long timestamp, OnTimerContext context, Collector<Long> out) throws Exception {
+            // Таймер срабатывает только в случае слишком долгой поездки
+            out.collect(rideState.value().rideId);
+
+            // Очистка состояния предотвращает дублирование предупреждений
+            rideState.clear();
+        }
+
+        private boolean rideTooLong(TaxiRide startEvent, TaxiRide endEvent) {
+            return Duration.between(startEvent.eventTime, endEvent.eventTime)
+                    .compareTo(Duration.ofHours(2)) > 0;
+        }
+
+        private long getTimerTime(TaxiRide ride) throws RuntimeException {
+            if (ride.isStart) {
+                return ride.eventTime.plusSeconds(120 * 60).toEpochMilli();
+            } else {
+                throw new RuntimeException("Невозможно получить время начала из события END.");
+            }
+        }
+    }
+}
