c7363be873f24f19d5f19baf510022e68a593c92
Andrey Yashakin <yashakin2073@gmail.com>
2023-12-24 21:58:31 +0400

    Сделал четвертую лабораторную

--- Lab4/scala/twoPhaze/Coordinator.scala
+++ Lab4/scala/twoPhaze/Coordinator.scala
@@ -11,7 +11,10 @@ case class Coordinator(hostPort:String,
   val zookeeper = new ZooKeeper(hostPort, 3000, this)
   val coordinatorPath = root + "/coordinator"
 
+
+  if (zookeeper == null) throw new Exception("ZK is NULL.")
   override def process(event: WatchedEvent): Unit = {
+    
   }
 
   def run(): Unit = {
@@ -20,19 +23,19 @@ case class Coordinator(hostPort:String,
     while(true) {
       val workers = zookeeper.getChildren(coordinatorPath, this)
       if (workers.size == n_workers) {
-        println("All workers voted")
+        println("All clients voted")
         var commits = 0
         var aborts = 0
         for (i <- 0 until n_workers) {
           val w = workers.get(i)
-          val data = new String(zookeeper.getData(s"$coordinatorPath/worker_$i", false, null))
+          val data = new String(zookeeper.getData(s"$coordinatorPath/client_$i", false, null))
           if (data == "commit") commits += 1
           else if (data == "abort") aborts += 1
         }
         val decision = if (commits > aborts) "commit" else "abort"
         for (i <- 0 until n_workers) {
           val w = workers.get(i)
-          zookeeper.setData(s"$coordinatorPath/worker_$i", decision.getBytes, -1)
+          zookeeper.setData(s"$coordinatorPath/client_$i", decision.getBytes, -1)
         }
         println(decision)
         while (true) {
